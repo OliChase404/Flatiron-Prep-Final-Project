7aa857bd2db2b27c7083e6d834f2d1eb421ba014
Oliver <olichase91@gmail.com>
2023-02-09 17:17:30 -0800

    A few tweaks

--- script.js
+++ script.js
@@ -1,42 +1,21 @@
 
+// // function navHome(){
+// //     window.open("https://www.youtube.com/")
+// //     console.log(`I was called`)
+// // };
+
+// const enterButton = document.getElementById("enterButton");
+
+// enterButton.addEventListener('click', function(){
+//     window.open("https://www.youtube.com/")
+// });
 
 
-function fizzbuzz(n){
-
-    let i = 1
-    const arr = []
-
-    while(i <= n){
-    if(i%5 === 0 && i%3 === 0){
-        arr.push(`FizzBuzz`)
-    }
-    else if(i%3 === 0){
-        arr.push(`Fizz`)
-    }
-    else if(i%5 === 0){
-        arr.push(`Buzz`)
-    }
-    else{
-        arr.push(i.toString())
-    }
-
-    i++
-    }
-    return arr
-}
-
-
-fizzbuzz(20)
-
-// console.log(arr)
-
-
-
-
-
-
-
-// const h2 = document.createElement("h2");
-// h2.textContent = "This content added by JavaScript";
-
-// document.querySelector("body").appendChild(h2);
+window.addEventListener("load", function () {
+    const enterButton = document.getElementById("enterButton");
+    enterButton.addEventListener('click', function(){
+      window.open("https://www.youtube.com/")
+    });
+  });
+  
+  
