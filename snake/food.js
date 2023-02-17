abd33c3bb82dc4780ec5a7f59b26076e7da0b539
Oliver <olichase91@gmail.com>
2023-02-17 10:49:27 -0800

    added score counter to snake

--- snake/food.js
+++ snake/food.js
@@ -4,11 +4,17 @@ import { onSnake, growSnake } from "./snake.js"
 
 let food = getRandomFoodPosition()
 const growthRate = 1
+let score = 0
+const scoreBoard = document.getElementById('scoreBoard')
+scoreBoard.innerHTML = (`Score: ${score}`)
 
+// Check if food has been eaten. If so, move food, increase and display score
 export function update() {
     if (onSnake(food)){
         growSnake(growthRate)
         food = getRandomFoodPosition()
+        score++
+        scoreBoard.innerHTML = (`Score: ${score}`)
     }
   }
   
