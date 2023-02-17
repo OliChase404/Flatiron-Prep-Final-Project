0519cf93901475f99c702fab973811bc6a93938b
Oliver <olichase91@gmail.com>
2023-02-16 16:52:54 -0800

    added player control to snake

--- snake/snake.js
+++ snake/snake.js
@@ -1,17 +1,18 @@
-export let snakeSpeed = 2
+import { getinputdirection } from "./playerControl.js"
+
+export let snakeSpeed = 4
 const snakeBody = [{x: 11, y: 11}]
 
 export function update() {
-  console.log("Update Snake")
+  const inputDirection = getinputdirection()
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = {...snakeBody[i] }
   }
-  snakeBody[0].x += 1
-  snakeBody[0].y += 0
+  snakeBody[0].x += inputDirection.x
+  snakeBody[0].y += inputDirection.y
 }
 
 export function draw(gameWindow) {
-  console.log("Draw Snake");
   snakeBody.forEach(segment => {
       const snakeElement = document.createElement('div')
       snakeElement.style.gridRowStart = segment.y
